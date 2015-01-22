9905257a06c9825706c03e9d661a1dc503f7213e
savanahmany <smany@student.dwc.edu>
2015-01-22 16:29:34 -0500

    deleted unused folders, created lab2

--- mascotLab/mascotLab/MyForm.h
+++ mascotLab/mascotLab/MyForm.h
@@ -14,6 +14,9 @@ namespace Project1 {
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
+	private:
+		Graphics^g;
+		Bitmap^ bmp = gcnew Bitmap("ncsu.bmp");
 	public:
 		MyForm(void)
 		{
@@ -35,11 +38,16 @@ namespace Project1 {
 			}
 		}
 
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Panel^  panel1;
+
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+	System::ComponentModel::Container ^components;
+	
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +56,60 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(356, 65);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(117, 59);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Toggle Visibility";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// panel1
+			// 
+			this->panel1->Location = System::Drawing::Point(35, 65);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(292, 290);
+			this->panel1->TabIndex = 2;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::panel1_Paint);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(508, 437);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+		bool vis = false;
+		int x = 0, y = 0;
+
+
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+				 g = panel1->CreateGraphics();
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 if (vis == true)
+				vis = false;
+				else vis = true;
+	}
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+				 if (vis == true)
+					 g->DrawImage(bmp, x, y);
+				 else Refresh();
+	}
+};
 }
